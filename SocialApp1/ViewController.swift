5c37b3ef7ce57c461215037e79744952d61dd4c8
Brian Lee <brianjacoblee@gmail.com>
2016-11-12 21:50:07 -0500

    Initial Commit

---
+++ SocialApp1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialApp1
+//
+//  Created by MacBook Air on 11/12/16.
+//  Copyright © 2016 Lee's. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
